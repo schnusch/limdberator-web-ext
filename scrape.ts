f47efb66e313ada12ece701a6c6bb05d406e67d5
schnusch <schnusch@users.noreply.github.com>
2022-08-05 23:29:45 +0200

    scrape directors and writers; scrape from /fullcredits

--- scrape.ts
+++ scrape.ts
@@ -8,6 +8,8 @@ export type ScrapedTitle = {
     rating?: string,
     rating_count?: number,
     year?: string,
+    directors?: CastMember[],
+    writers?: CastMember[],
     cast?: CastMember[],
     duration?: number,
     languages?: string[],
@@ -167,9 +169,64 @@ export function get_release_year(): string|null {
     return year.textContent.normalize_whitespace()
 }
 
+function get_fullcredits(heading_selector: string): CastMember[] {
+    const table = document.querySelector(`${heading_selector} ~ table`)
+    if(!table) {
+        return []
+    }
+    const people: CastMember[] = []
+    for(const tr of table.querySelectorAll("tr")) {
+        const a = tr.querySelector("td:not(.primary_photo) a")
+        const id = a?.getAttribute("href")?.match(/name\/(nm\d+)/)
+        const name = a?.textContent
+        if(id && name) {
+            people.push([id[1], name.normalize_whitespace()])
+        }
+    }
+    return people
+}
+
+export function get_directors(): CastMember[] {
+    const directors = get_fullcredits("#director")
+    if(directors.length > 0) {
+        return directors
+    }
+    const director_list = document.querySelector('[href*="fullcredits/director"]')?.parentElement?.querySelector("ul")
+    if(!director_list) {
+        return []
+    }
+    for(const a of director_list.querySelectorAll("[href]")) {
+        const id = a.getAttribute("href")?.match(/name\/(nm\d+)/)
+        if(id && a.textContent) {
+            directors.push([id[1], a.textContent.normalize_whitespace()])
+        }
+    }
+    return directors
+}
+
+export function get_writers(): CastMember[] {
+    const writers = get_fullcredits("#writer")
+    if(writers.length > 0) {
+        return writers
+    }
+    const writer_list = document.querySelector('[href*="fullcredits/writer"]')?.parentElement?.querySelector("ul")
+    if(!writer_list) {
+        return []
+    }
+    for(const a of writer_list.querySelectorAll("[href]")) {
+        const id = a.getAttribute("href")?.match(/name\/(nm\d+)/)
+        if(id && a.textContent) {
+            writers.push([id[1], a.textContent.normalize_whitespace()])
+        }
+    }
+    return writers
+}
+
 export function get_cast(): CastMember[] {
-    // TODO handle .../fullcredits
-    const cast: Array<[string, string]> = []
+    const cast: CastMember[] = get_fullcredits("#cast")
+    if(cast.length > 0) {
+        return cast
+    }
     for(const a of document.querySelectorAll('[data-testid="title-cast-item"] [data-testid="title-cast-item__actor"]')) {
         const id = a.getAttribute("href")?.match(/name\/(nm\d+)/)
         if(id && a.textContent) {
@@ -288,14 +345,18 @@ export function scrape(): ScrapeResult|null {
             id: id,
             timestamp: Math.floor(Date.now() / 1000),
         }
-        set_if_not_null(result, "title",          get_title())
-        set_if_not_null(result, "original_title", get_original_title())
-        set_if_not_null(result, "rating",         get_rating())
-        set_if_not_null(result, "rating_count",   get_rating_count())
-        set_if_not_null(result, "year",           get_release_year())
-        set_if_not_null(result, "cast",           get_cast())
-        set_if_not_null(result, "duration",       get_duration())
-        set_if_not_null(result, "languages",      get_languages())
+        if(location.pathname.indexOf("/fullcredits") < 0) {
+            set_if_not_null(result, "title",          get_title())
+            set_if_not_null(result, "original_title", get_original_title())
+            set_if_not_null(result, "rating",         get_rating())
+            set_if_not_null(result, "rating_count",   get_rating_count())
+            set_if_not_null(result, "year",           get_release_year())
+            set_if_not_null(result, "duration",       get_duration())
+            set_if_not_null(result, "languages",      get_languages())
+        }
+        set_if_not_null(result, "directors", get_directors())
+        set_if_not_null(result, "writers",   get_writers())
+        set_if_not_null(result, "cast",      get_cast())
         if(Object.keys(result).length > 2) {
             return {title: result}
         }
